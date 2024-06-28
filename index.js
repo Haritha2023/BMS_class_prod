04d71457419bf4d84c8861b0fccb4b05bf851bab
Haritha2023 <haritha.sadanala@outlook.com>
2024-06-28 15:43:17 +0530

    production

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+var cors = require("cors");
+
+require("dotenv").config();
+
+const dbConfig = require("./config/dbConfig");
+
+const PORT = 8088;
+
+const app = express();
+
+const userRoutes = require("./routes/userRoutes");
+const movieRoutes = require("./routes/movieRoutes");
+const theatreRoutes = require("./routes/theatreRoutes");
+const showRoutes = require("./routes/showRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+app.use("/api/users", userRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/theatres", theatreRoutes);
+app.use("/api/shows", showRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.listen(PORT, () => {
+  console.log("server running");
+});
